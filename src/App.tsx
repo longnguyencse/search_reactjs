34b02dbe76eb66bc0406508b869aacc3ce8c42ad
Nhan <trongnhanv63@gmail.com>
2019-08-15 00:20:57 +0700

    create layout with antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import {Avatar, Col, Input, List, Row} from 'antd';
-import {IconText} from './component/IconText';
+import { Avatar, Col, Input, List, Row, Layout, Menu, Icon, Breadcrumb } from 'antd';
+
+import { IconText } from './component/IconText';
 import axios from 'axios';
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Index from './pages/ProductOrder';
 import Detail from './pages/ProductOrder/detail';
 
+import LeftMenu from './components/LeftMenu';
+import MainContent from './components/MainContent';
 import './App.css';
-import {API} from "./service/API";
+import { API } from "./service/API";
 
-const {Search} = Input;
-class App extends React.Component<{}, {}> {
+const { Search } = Input;
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+class App extends React.Component<IAppProps, IAppState> {
 
     private listData: any[] = new Array();
 
@@ -28,7 +34,16 @@ class App extends React.Component<{}, {}> {
         //             'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
         //     });
         // }
+
+        this.state = {
+            collapsed: false,
+        };
     }
+
+    onCollapse = (collapsed: boolean) => {
+        console.log(collapsed);
+        this.setState({ collapsed });
+    };
 
     componentDidMount(): void {
         // axios.get('http://localhost:5200/redmine').then(res => {
@@ -45,6 +60,15 @@ class App extends React.Component<{}, {}> {
 
     render() {
         return (
+            <Layout>
+                <LeftMenu></LeftMenu>
+                <Layout style={{ marginLeft: 200 }}>
+                    <MainContent></MainContent>
+                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                </Layout>
+            </Layout>
+        );
+        return (
             <main className="contaier">
                 <Router>
                     <Switch>
@@ -58,7 +82,7 @@ class App extends React.Component<{}, {}> {
             <section className="App">
                 <Row type="flex" justify="center">
                     <Col span={16}>
-                        <Search placeholder="input search text" onChange={value => console.log(value)} enterButton/>
+                        <Search placeholder="input search text" onChange={value => console.log(value)} enterButton />
                     </Col>
                     <Col span={16}>
                         <List itemLayout="vertical" size="large" pagination={{
@@ -67,40 +91,48 @@ class App extends React.Component<{}, {}> {
                             },
                             pageSize: 3,
                         }}
-                              dataSource={this.listData}
-                              footer={
-                                  <div>
-                                      <b>ant design</b> footer part
+                            dataSource={this.listData}
+                            footer={
+                                <div>
+                                    <b>ant design</b> footer part
                                   </div>
-                              }
-                              renderItem={item => (
-                                  <List.Item
-                                      key={item.title}
-                                      actions={[
-                                          <IconText text={12}/>,
-                                          <IconText text={'long'}/>,
-                                          <IconText text={'adayroi'}/>,
-                                      ]}
-                                      extra={
-                                          <img
-                                              width={272}
-                                              alt="logo"
-                                              src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                                          />
-                                      }
-                                  >
-                                      <List.Item.Meta
-                                          avatar={<Avatar src={item.avatar}/>}
-                                          title={<a href={item.href}>{item.title}</a>}
-                                          description={item.description}
-                                      />
-                                      {item.content}
-                                  </List.Item>
-                              )}/>
+                            }
+                            renderItem={item => (
+                                <List.Item
+                                    key={item.title}
+                                    actions={[
+                                        <IconText text={12} />,
+                                        <IconText text={'long'} />,
+                                        <IconText text={'adayroi'} />,
+                                    ]}
+                                    extra={
+                                        <img
+                                            width={272}
+                                            alt="logo"
+                                            src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                                        />
+                                    }
+                                >
+                                    <List.Item.Meta
+                                        avatar={<Avatar src={item.avatar} />}
+                                        title={<a href={item.href}>{item.title}</a>}
+                                        description={item.description}
+                                    />
+                                    {item.content}
+                                </List.Item>
+                            )} />
                     </Col>
                 </Row>
             </section>
         )
     }
+}
+
+interface IAppProps {
+
+}
+
+interface IAppState {
+    collapsed: boolean
 }
 export default App;
---
+++ src/components/LeftMenu/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {Layout, Menu, Icon} from 'antd';
+
+import './style.css';
+const {Sider} = Layout;
+
+export default class LeftMenu extends React.Component<ILeftMenuProps, ILeftMenuState> {
+    constructor(props: ILeftMenuProps){
+        super(props);
+    }
+
+    render(){
+        return (
+            <Sider className="left-sider">
+                <Menu>
+                    {/* <div className="logo">
+                        <img src="https://images.unsplash.com/photo-1533407411655-dcce1534c1a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80" alt="" width="100px"/>
+                    </div> */}
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                        <Menu.Item key="1">
+                            <Icon type="appstore-o" />
+                            <span className="nav-text">Product Order</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="user" />
+                            <span className="nav-text">Test</span>
+                        </Menu.Item>
+                        
+                    </Menu>
+                </Menu>
+            </Sider>
+        );
+    }
+};
+
+interface ILeftMenuProps {
+
+}
+
+interface ILeftMenuState {
+
+}
