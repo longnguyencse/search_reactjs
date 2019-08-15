bb6e4eeb8dfb058a34e2d47bbb36cc56b77e0989
Nhan <trongnhanv63@gmail.com>
2019-08-15 08:49:11 +0700

    Table

--- src/pages/ProductOrder/list.tsx
+++ src/pages/ProductOrder/list.tsx
@@ -1,33 +1,104 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 
+import {Table} from 'antd';
+
+const columns = [
+    {
+        title: "Ngay tao",
+        dataIndex: "ngayTao",
+    },
+    {
+        title: "Ngay giao hang",
+        dataIndex: "ngayGiaoHang",
+    },
+    {
+        title: "Ma PO",
+        dataIndex: "maPO",
+    },
+    {
+        title: "Nha cung cap",
+        dataIndex: "nhaCungCap",
+    },
+    {
+        title: "Ten san pham",
+        dataIndex: "tenSanPham",
+    },
+    {
+        title: "So luong",
+        dataIndex: "soLuong",
+    },
+    {
+        title: "Don vi",
+        dataIndex: "donVi",
+    },
+    {
+        title: "Thanh tien",
+        dataIndex: "thanhTien",
+    },
+    {
+        title: "Action",
+        dataIndex: "action",
+    },
+    {
+        title: "Chon tat ca",
+        dataIndex: "chonTatCa",
+    },
+    {
+        title: "Ngay tao",
+        dataIndex: "ngayTao",
+    },
+];
+
 export default class List extends React.Component<IListProps, IListState> {
+    private data: any[] = new Array();
+
     constructor(props: IListProps){
         super(props);
 
+        for(let i = 0 ; i < 46 ; i++){
+            const soLuong = i + 1;
+            const thanhTien = 1000 * soLuong;
+            this.data.push(
+                {
+                    key: i,
+                    ngayTao: "1/1/2019",
+                    ngayGiaoHang: "2/1/2019",
+                    maPO: `maPO-${i}`,
+                    nhaCungCap: `nhaCungCap-${i}`,
+                    tenSanPham: `tenSanPham-${i}`,
+                    soLuong,
+                    donVi: "Gi Cung Duoc",
+                    thanhTien,
+                    trangThai: "Cho Duyet"
+                }
+            );
+        }
+
         this.state = {
-            names: ['A', 'B', 'C', 'D']
+            selectedRowKeys: [],
         };
     }
 
+    onSelectChange = ( selectedRowKeys: Array<Object> ) => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    this.setState({ selectedRowKeys });
+    };
+
     render(){
-        return (
-            <ul className="list-group">
-                {this.state.names.map((name, index) => {
-                    console.log(index)
-                    return <li key={index} className="list-group-item">
-                        <Link to={`po/${name}`}>{name}</Link>
-                    </li>
-                })}
-            </ul>
-        );
+        const {selectedRowKeys} = this.state;
+        const rowSelection = {
+            selectedRowKeys,
+            onChange: this.onSelectChange,
+            hideDefaultSelections: true
+        }
+        return <Table columns={columns} dataSource={this.data} />;
     }
 };
 
 interface IListProps {
-
 }
 
 interface IListState {
-    names: Array<string>
+    selectedRowKeys: Array<Object>
 }
