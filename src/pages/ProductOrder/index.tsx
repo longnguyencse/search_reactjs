699f90521ee8a45261dbce19f846ac8abe4021fa
Long Nguyen <longnq@itvsoft.vn>
2019-08-17 23:23:43 +0700

    login

--- src/pages/ProductOrder/index.tsx
+++ src/pages/ProductOrder/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-import { Route } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 import List from './list';
 import Detail from './detail';
+import Login from "../Login/Login";
 
 export default class ProductOrder extends React.Component<IProductOrderProps, IProductOrderState> {
     constructor(props: IProductOrderProps){
@@ -16,6 +17,7 @@ export default class ProductOrder extends React.Component<IProductOrderProps, IP
                 <Route exact path="/" component={List} />
                 <Route exact path="/po" component={List} />
                 <Route exact path="/po/:name" component={Detail} />
+                <Route exact path="/login" component={Login}/>
             </div>
         );
     }
