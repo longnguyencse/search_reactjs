752b88b6ef5462a3c7b3fcb4fd5bc2e91a49cb57
Long Nguyen <longnq@itvsoft.vn>
2019-09-14 23:45:15 +0700

    change config

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,6 +1,6 @@
 // const baseUrl = "http://192.168.68.149:8080/api";
-const baseUrl = "http://localhost:8080/api";
-
+// const baseUrl = "http://localhost:8080/api";
+const baseUrl = "http://mystore:8080/api";
 export default {
     apiLogin: baseUrl + "/oauth2/signin",
     apiLogout: baseUrl + "/oauth2/logout",
