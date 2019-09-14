b218777baf8801b6d7496d0a0b842db050ee44d3
Long Nguyen <longnq@itvsoft.vn>
2019-09-15 00:28:57 +0700

    change address

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,7 +1,7 @@
 // const baseUrl = "http://192.168.68.149:8080/api";
 // const baseUrl = "http://localhost:8080/api";
 // const baseUrl = "http://mystore:8080/api";
-const baseUrl = "http://35.187.243.39:8080:8080/api";
+const baseUrl = "http://35.187.243.39:8080/api";
 export default {
     apiLogin: baseUrl + "/oauth2/signin",
     apiLogout: baseUrl + "/oauth2/logout",
