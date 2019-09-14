1f626f4e1d37b61b8299dc2fa96434bf9471bca3
Long Nguyen <longnq@itvsoft.vn>
2019-09-14 21:56:51 +0700

    translate class

--- src/components/LeftMenu/index.tsx
+++ src/components/LeftMenu/index.tsx
@@ -147,44 +147,12 @@ class LeftMenu extends React.Component<Props, ILeftMenuState> {
                             <Link to="/user/profile">Thông Tin</Link></Menu.Item>
                         <Menu.Item key="12">
                             {/*<Link to="/logout">Thoát</Link>*/}
-                            <Button type="danger" onClick={this.handleLogout}>Logout</Button>
+                            <Button type="danger" onClick={this.handleLogout}>Thoát</Button>
                         </Menu.Item>
                     </SubMenu>
                 </Menu>
                 
 
-            </Sider>
-        );
-        return (
-            <Sider className="left-sider">
-                <Menu>
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-                        <Menu.Item key="1">
-                            <Icon type="appstore-o" />
-                            <span className="nav-text">
-                                <Link to="/po">Product Order</Link>
-                            </span>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Icon type="user" />
-                            <span className="nav-text">Test</span>
-                        </Menu.Item>
-                        <SubMenu
-                            key="sub4"
-                            title={
-                                <span>
-                <Icon type="setting"/>
-                <span>Navigation Three</span>
-              </span>
-                            }
-                        >
-                            <Menu.Item key="9">Option 9</Menu.Item>
-                            <Menu.Item key="10">Option 10</Menu.Item>
-                            <Menu.Item key="11">Option 11</Menu.Item>
-                            <Menu.Item key="12">Option 12</Menu.Item>
-                        </SubMenu>
-                    </Menu>
-                </Menu>
             </Sider>
         );
     }
